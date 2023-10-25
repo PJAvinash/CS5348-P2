d557fc9d9c8e938385de2fc0734cdc0343fd21b7
PJ Avinash <avinashindian2.0@gmail.com>
2023-10-25 14:19:53 -0500

    modified user/makefile for tests

--- xv6/user/makefile.mk
+++ xv6/user/makefile.mk
@@ -1,24 +1,13 @@
 
+# modified by JXP220032 
 # user programs
-USER_PROGS := \
-	cat\
-	echo\
-	forktest\
-	grep\
-	init\
-	kill\
-	ln\
-	ls\
-	mkdir\
-	rm\
-	sh\
-	stressfs\
-	tester\
-	usertests\
-	wc\
-	zombie
+USER_PROGS := $(wildcard user/testcases/*.c)
 
-USER_PROGS := $(addprefix user/, $(USER_PROGS))
+# Append the user programs you want to keep from the old list
+USER_PROGS += cat echo forktest grep init kill ln ls mkdir rm sh stressfs tester usertests wc zombie
+
+USER_PROGS := $(addprefix user/, $(notdir $(USER_PROGS)))
+
 
 # user library files
 USER_LIBS := \
